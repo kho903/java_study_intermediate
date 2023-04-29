2ecc7c4e2da6fee9918a1d1c934b3b4ffc8b8b38
Kim Ji Hun <gmldnr2222@naver.com>
2023-04-29 19:24:01 +0900

    feat: Class 클래스와 Reflection programming
    - Person 클래스
    - Class 클래스

---
+++ src/main/java/ch04/ClassTest.java
@@ -0,0 +1,22 @@
+package ch04;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class ClassTest {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		Class c	= Class.forName("java.lang.String");
+		System.out.println(c.getName());
+		Constructor<String>[] constructors = c.getConstructors();
+
+		for (Constructor<String> constructor : constructors) {
+			System.out.println(constructor);
+		}
+
+		Method[] methods = c.getMethods();
+		for (Method method : methods) {
+			System.out.println(method);
+		}
+	}
+}
---
+++ src/main/java/ch04/PersonClassTest.java
@@ -0,0 +1,20 @@
+package ch04;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class PersonClassTest {
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException,
+			InvocationTargetException, InstantiationException, IllegalAccessException {
+		Person person = new Person("James");
+		System.out.println(person);
+
+		Class c = Class.forName("ch04.Person");
+		Class[] parameterTypes = {String.class};
+		Constructor constructor = c.getConstructor(parameterTypes);
+
+		Object[] initArgs = {"김지훈"};
+		Person personKim = (Person)constructor.newInstance(initArgs);
+		System.out.println(personKim);
+	}
+}
